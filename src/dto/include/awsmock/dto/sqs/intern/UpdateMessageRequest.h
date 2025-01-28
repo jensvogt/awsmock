2ccb3eb4542861dc3d78c5fa9e869bd378280778
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-01-28 23:55:26 +0100

    fix(awsmock-910-lambda-code-upload): fix DQL subscription validation

--- src/dto/include/awsmock/dto/sqs/intern/UpdateMessageRequest.h
+++ src/dto/include/awsmock/dto/sqs/intern/UpdateMessageRequest.h
@@ -115,6 +115,6 @@ namespace AwsMock::Dto::SQS {
          */
         friend std::ostream &operator<<(std::ostream &os, UpdateMessageRequest &r);
     };
-}// namespace AwsMock::Dto::SQS
+} // namespace AwsMock::Dto::SQS
 
 #endif// AWSMOCK_DTO_SQS_UPDATE_MESSAGE_REQUEST_H
