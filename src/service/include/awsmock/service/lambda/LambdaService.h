91d3229c5b3a25ddc35cf0fe076f0e9b8394901a
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-02-04 19:20:52 +0100

    fix(861-lambda-invocation): fix lambda instance cleanup

--- src/service/include/awsmock/service/lambda/LambdaService.h
+++ src/service/include/awsmock/service/lambda/LambdaService.h
@@ -92,7 +92,7 @@ namespace AwsMock::Service {
         /**
          * @brief Constructor
          */
-        explicit LambdaService() : _lambdaDatabase(Database::LambdaDatabase::instance()){};
+        explicit LambdaService() : _lambdaDatabase(Database::LambdaDatabase::instance()) {};
 
         /**
          * @brief Create lambda function
@@ -278,7 +278,7 @@ namespace AwsMock::Service {
          *
          * @param lambda lambda entity to cleanup
          */
-        static void CleanupDocker(const Database::Entity::Lambda::Lambda &lambda);
+        static void CleanupDocker(Database::Entity::Lambda::Lambda &lambda);
 
         /**
          * @brief Returns the host name, to where we send lambda invocation notifications
