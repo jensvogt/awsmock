d32dd957bc0f7b9242e9b44e06676b6d81c48829
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-05-31 11:15:37 +0200

    develop(1251-show-lambda-logs): fix tests

--- src/service/btst/SNSServiceJavaTests.cpp
+++ src/service/btst/SNSServiceJavaTests.cpp
@@ -2,12 +2,6 @@
 // Created by vogje01 on 21/10/2023.
 //
 
-#ifndef AWMOCK_SERVICE_SNS_JAVA_SERVER_TEST_H
-#define AWMOCK_SERVICE_SNS_JAVA_SERVER_TEST_H
-
-// GTest includes
-#include <gtest/gtest.h>
-
 // AwsMock includes
 #include "TestBase.h"
 #include <awsmock/core/FileUtils.h>
@@ -32,11 +26,11 @@ namespace AwsMock::Service {
      *
      * @author jens.vogt\@opitz-consulting.com
      */
-    class SNSServerJavaTest : public testing::Test, public TestBase {
+    struct SNSServiceJavaTest : TestBase {
 
       protected:
 
-        void SetUp() override {
+        SNSServiceJavaTest() {
 
             // Start gateway server
             StartGateway(TEST_PORT);
@@ -47,17 +41,16 @@ namespace AwsMock::Service {
             // Base URL
             _snsBaseUrl = "/api/sns/";
             _sqsBaseUrl = "/api/sqs/";
-        }
 
-        void TearDown() override {
-            long count = _snsDatabase.DeleteAllMessages();
-            log_info << "SNS message deleted, count: " << count;
-            count = _snsDatabase.DeleteAllTopics();
-            log_info << "SNS topics deleted, count: " << count;
-            count = _sqsDatabase.DeleteAllMessages();
-            log_info << "SQS messages deleted, count: " << count;
-            count = _sqsDatabase.DeleteAllQueues();
-            log_info << "SQS queues deleted, count: " << count;
+            // Cleanup
+            // ReSharper disable once CppExpressionWithoutSideEffects
+            _snsDatabase.DeleteAllMessages();
+            // ReSharper disable once CppExpressionWithoutSideEffects
+            _snsDatabase.DeleteAllTopics();
+            // ReSharper disable once CppExpressionWithoutSideEffects
+            _sqsDatabase.DeleteAllMessages();
+            // ReSharper disable once CppExpressionWithoutSideEffects
+            _sqsDatabase.DeleteAllQueues();
         }
 
         static Core::HttpSocketResponse SendPostCommand(const std::string &url, const std::string &payload) {
@@ -84,16 +77,13 @@ namespace AwsMock::Service {
             return response;
         }
 
-        boost::thread _thread;
         std::string _snsBaseUrl, _sqsBaseUrl, _region;
-        boost::asio::io_context _ios{10};
-        Core::Configuration &_configuration = Core::Configuration::instance();
         Database::SNSDatabase &_snsDatabase = Database::SNSDatabase::instance();
         Database::SQSDatabase &_sqsDatabase = Database::SQSDatabase::instance();
         std::shared_ptr<GatewayServer> _gatewayServer;
     };
 
-    TEST_F(SNSServerJavaTest, TopicCreateTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicCreateTest, SNSServiceJavaTest) {
 
         // arrange
 
@@ -102,54 +92,54 @@ namespace AwsMock::Service {
         const std::string topicArn = result.body;
 
         // assert
-        EXPECT_TRUE(result.statusCode == http::status::ok);
-        EXPECT_FALSE(topicArn.empty());
-        EXPECT_TRUE(Core::StringUtils::Contains(topicArn, TEST_TOPIC));
+        BOOST_CHECK_EQUAL(result.statusCode == http::status::ok, true);
+        BOOST_CHECK_EQUAL(topicArn.empty(), false);
+        BOOST_CHECK_EQUAL(Core::StringUtils::Contains(topicArn, TEST_TOPIC), true);
     }
 
-    TEST_F(SNSServerJavaTest, TopicListTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicListTest, SNSServiceJavaTest) {
 
         // arrange
         const Core::HttpSocketResponse result = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
-        EXPECT_TRUE(result.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(result.statusCode == http::status::ok, true);
         std::string topicArn = result.body;
 
         // act
         const Core::HttpSocketResponse listResult = SendGetCommand(_snsBaseUrl + "listTopics", {});
-        EXPECT_TRUE(listResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(listResult.statusCode == http::status::ok, true);
 
         // assert
-        EXPECT_EQ(1, std::stoi(listResult.body));
+        BOOST_CHECK_EQUAL(1, std::stoi(listResult.body));
     }
 
-    TEST_F(SNSServerJavaTest, TopicSubscribeTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicSubscribeTest, SNSServiceJavaTest) {
 
         // arrange
-        Core::HttpSocketResponse createTopicResult = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
-        EXPECT_TRUE(createTopicResult.statusCode == http::status::ok);
-        std::string topicArn = createTopicResult.body;
-        Core::HttpSocketResponse createQueueResult = SendPostCommand(_sqsBaseUrl + "createQueue?queueName=" + Core::StringUtils::UrlEncode(TEST_QUEUE), {});
-        EXPECT_TRUE(createQueueResult.statusCode == http::status::ok);
-        std::string queueUrl = createQueueResult.body;
+        const Core::HttpSocketResponse createTopicResult = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
+        BOOST_CHECK_EQUAL(createTopicResult.statusCode == http::status::ok, true);
+        const std::string topicArn = createTopicResult.body;
+        const Core::HttpSocketResponse createQueueResult = SendPostCommand(_sqsBaseUrl + "createQueue?queueName=" + Core::StringUtils::UrlEncode(TEST_QUEUE), {});
+        BOOST_CHECK_EQUAL(createQueueResult.statusCode == http::status::ok, true);
+        const std::string queueUrl = createQueueResult.body;
 
         // act
-        Core::HttpSocketResponse subscribeResult = SendPostCommand(_snsBaseUrl + "subscribe?topicArn=" + Core::StringUtils::UrlEncode(topicArn) + "&queueUrl=" + Core::StringUtils::UrlEncode(queueUrl) + "&protocol=SQS", {});
-        std::string subscriptionArn = subscribeResult.body;
+        const Core::HttpSocketResponse subscribeResult = SendPostCommand(_snsBaseUrl + "subscribe?topicArn=" + Core::StringUtils::UrlEncode(topicArn) + "&queueUrl=" + Core::StringUtils::UrlEncode(queueUrl) + "&protocol=SQS", {});
+        const std::string subscriptionArn = subscribeResult.body;
 
         // assert
-        EXPECT_TRUE(subscribeResult.statusCode == http::status::ok);
-        EXPECT_FALSE(subscriptionArn.empty());
-        EXPECT_TRUE(Core::StringUtils::Contains(subscriptionArn, "test-topic"));
+        BOOST_CHECK_EQUAL(subscribeResult.statusCode == http::status::ok, true);
+        BOOST_CHECK_EQUAL(subscriptionArn.empty(), false);
+        BOOST_CHECK_EQUAL(Core::StringUtils::Contains(subscriptionArn, "test-topic"), true);
     }
 
-    TEST_F(SNSServerJavaTest, TopicUnsubscribeTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicUnsubscribeTest, SNSServiceJavaTest) {
 
         // arrange
         Core::HttpSocketResponse createTopicResult = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
-        EXPECT_TRUE(createTopicResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(createTopicResult.statusCode == http::status::ok, true);
         std::string topicArn = createTopicResult.body;
         Core::HttpSocketResponse createQueueResult = SendPostCommand(_sqsBaseUrl + "createQueue?queueName=" + Core::StringUtils::UrlEncode(TEST_QUEUE), {});
-        EXPECT_TRUE(createQueueResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(createQueueResult.statusCode == http::status::ok, true);
         std::string queueUrl = createQueueResult.body;
         Core::HttpSocketResponse subscribeResult = SendPostCommand(_snsBaseUrl + "subscribe?topicArn=" + Core::StringUtils::UrlEncode(topicArn) + "&queueUrl=" + Core::StringUtils::UrlEncode(queueUrl) + "&protocol=SQS", {});
         std::string subscriptionArn = subscribeResult.body;
@@ -158,31 +148,31 @@ namespace AwsMock::Service {
         Core::HttpSocketResponse unsubscribeResult = SendPostCommand(_snsBaseUrl + "unsubscribe?subscriptionArn=" + Core::StringUtils::UrlEncode(subscriptionArn), {});
 
         // assert
-        EXPECT_TRUE(unsubscribeResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(unsubscribeResult.statusCode == http::status::ok, true);
     }
 
-    TEST_F(SNSServerJavaTest, TopicPublishTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicPublishTest, SNSServiceJavaTest) {
 
         // arrange
         const Core::HttpSocketResponse createTopicResult = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
-        EXPECT_TRUE(createTopicResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(createTopicResult.statusCode == http::status::ok, true);
         const std::string topicArn = createTopicResult.body;
 
         // act
         const Core::HttpSocketResponse publishResult = SendPostCommand(_snsBaseUrl + "publish?topicArn=" + Core::StringUtils::UrlEncode(topicArn), Core::StringUtils::UrlEncode(TEST_MESSAGE_JSON));
-        EXPECT_TRUE(publishResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(publishResult.statusCode == http::status::ok, true);
         const Database::Entity::SNS::MessageList messageList = _snsDatabase.ListMessages();
 
         // assert
-        EXPECT_TRUE(publishResult.statusCode == http::status::ok);
-        EXPECT_EQ(1, messageList.size());
+        BOOST_CHECK_EQUAL(publishResult.statusCode == http::status::ok, true);
+        BOOST_CHECK_EQUAL(1, messageList.size());
     }
 
-    TEST_F(SNSServerJavaTest, TopicDeleteTest) {
+    BOOST_FIXTURE_TEST_CASE(TopicDeleteTest, SNSServiceJavaTest) {
 
         // arrange
         const Core::HttpSocketResponse createTopicResult = SendPostCommand(_snsBaseUrl + "createTopic?name=" + Core::StringUtils::UrlEncode(TEST_TOPIC), {});
-        EXPECT_TRUE(createTopicResult.statusCode == http::status::ok);
+        BOOST_CHECK_EQUAL(createTopicResult.statusCode == http::status::ok, true);
         const std::string topicArn = createTopicResult.body;
 
         // act
@@ -190,10 +180,8 @@ namespace AwsMock::Service {
         const Database::Entity::SNS::TopicList topicList = _snsDatabase.ListTopics();
 
         // assert
-        EXPECT_TRUE(deleteResult.statusCode == http::status::ok);
-        EXPECT_EQ(0, topicList.size());
+        BOOST_CHECK_EQUAL(deleteResult.statusCode == http::status::ok, true);
+        BOOST_CHECK_EQUAL(0, topicList.size());
     }
 
 }// namespace AwsMock::Service
-
-#endif// AWMOCK_SERVICE_SNS_JAVA_SERVER_TEST_H
