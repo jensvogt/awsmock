d95cb9ef18a4fefce342c903ad497a0fc1037c43
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-06-29 16:23:06 +0200

    fix(1289-backup count): fix lambda startup

--- src/dto/include/awsmock/dto/sqs/DeleteMessageBatchResponse.h
+++ src/dto/include/awsmock/dto/sqs/DeleteMessageBatchResponse.h
@@ -18,20 +18,13 @@
 #include <awsmock/dto/sqs/model/DeleteMessageBatchResultEntry.h>
 
 namespace AwsMock::Dto::SQS {
-    struct DeleteMessageBatchResultEntry;
-    struct BatchResultErrorEntry;
 
-    struct DeleteMessageBatchResponse {
+    struct DeleteMessageBatchResponse final : Common::BaseCounter<DeleteMessageBatchResponse> {
 
         /**
          * Resource
          */
         std::string resource = "SQS";
-
-        /**
-         * Resource
-         */
-        std::string requestId = Core::StringUtils::CreateRandomUuid();
 
         /**
          * Failed
@@ -41,35 +34,78 @@ namespace AwsMock::Dto::SQS {
         /**
          * Successful
          */
-        std::vector<DeleteMessageBatchResultEntry> successFull;
-
-        /**
-         * Convert to a JSON string
-         *
-         * @return JSON string
-         */
-        [[nodiscard]] std::string ToJson() const;
+        std::vector<DeleteMessageBatchResultEntry> successfull;
 
         /**
          * Convert to XML representation
          *
          * @return XML string
          */
-        [[nodiscard]] std::string ToXml() const;
+        [[nodiscard]] std::string ToXml() const {
 
-        /**
-         * Converts the DTO to a string representation.
-         *
-         * @return DTO as string
-         */
-        [[nodiscard]] std::string ToString() const;
+            try {
 
-        /**
-         * Stream provider.
-         *
-         * @return output stream
-         */
-        friend std::ostream &operator<<(std::ostream &os, const DeleteMessageBatchResponse &r);
+                boost::property_tree::ptree pt;
+
+                // Failed
+                if (failed.empty()) {
+                    boost::property_tree::ptree failedArray;
+                    for (const auto &f: failed) {
+                        boost::property_tree::ptree element;
+                        element.put("Id", f.id);
+                        element.put("Code", f.code);
+                        element.put("SenderFault", f.senderFault);
+                        element.put("Message", f.message);
+                        failedArray.push_back(std::make_pair("", element));
+                    }
+                    pt.add_child("DeleteMessageBatchResponse.DeleteMessageBatchResult.Failed", failedArray);
+                }
+
+                // Successful
+                if (successfull.empty()) {
+                    boost::property_tree::ptree successArray;
+                    for (const auto &s: successfull) {
+                        boost::property_tree::ptree element;
+                        element.put("Id", s.id);
+                        successArray.push_back(std::make_pair("", element));
+                    }
+                    pt.add_child("DeleteMessageBatchResponse.DeleteMessageBatchResult.Successful", successArray);
+                }
+
+                // Metadata
+                pt.put("DeleteMessageBatchResponse.ResponseMetadata.RequestId", requestId);
+                return Core::XmlUtils::ToXmlString(pt);
+
+            } catch (bsoncxx::exception &exc) {
+                log_error << exc.what();
+                throw Core::JsonException(exc.what());
+            }
+        }
+
+      private:
+
+        friend DeleteMessageBatchResponse tag_invoke(boost::json::value_to_tag<DeleteMessageBatchResponse>, boost::json::value const &v) {
+            DeleteMessageBatchResponse r;
+            r.resource = Core::Json::GetStringValue(v, "Resource");
+            if (Core::Json::AttributeExists(v, "Successful")) {
+                r.successfull = boost::json::value_to<std::vector<DeleteMessageBatchResultEntry>>(v.at("Successful"));
+            }
+            if (Core::Json::AttributeExists(v, "Failed")) {
+                r.failed = boost::json::value_to<std::vector<BatchResultErrorEntry>>(v.at("Failed"));
+            }
+            return r;
+        }
+
+        friend void tag_invoke(boost::json::value_from_tag, boost::json::value &jv, DeleteMessageBatchResponse const &obj) {
+            jv = {
+                    {"Region", obj.region},
+                    {"User", obj.user},
+                    {"RequestId", obj.requestId},
+                    {"Resource", obj.resource},
+                    {"Successful", boost::json::value_from(obj.successfull)},
+                    {"Failed", boost::json::value_from(obj.failed)},
+            };
+        }
     };
 
 }// namespace AwsMock::Dto::SQS
