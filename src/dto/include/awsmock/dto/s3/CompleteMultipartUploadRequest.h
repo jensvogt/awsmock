4a90045f90ec69d0dee71406dda4d8a4ae96a492
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-08-03 23:57:03 +0200

    fix(1366-application-stop): add dynamodb streams

--- src/dto/include/awsmock/dto/s3/CompleteMultipartUploadRequest.h
+++ src/dto/include/awsmock/dto/s3/CompleteMultipartUploadRequest.h
@@ -9,17 +9,18 @@
 #include <string>
 
 // AwsMock includes
-#include <awsmock/core/BsonUtils.h>
+#include <awsmock/core/JsonUtils.h>
 #include <awsmock/core/XmlUtils.h>
+#include <awsmock/dto/common/BaseCounter.h>
 
 namespace AwsMock::Dto::S3 {
 
-    struct CompleteMultipartUploadRequest {
-
-        /**
-         * Region
-         */
-        std::string region;
+    /**
+     * @brief Complete multipart upload
+     *
+     * @author jens.vogt\@opitz-consulting.com
+     */
+    struct CompleteMultipartUploadRequest final : Common::BaseCounter<CompleteMultipartUploadRequest> {
 
         /**
          * Bucket
@@ -64,7 +65,7 @@ namespace AwsMock::Dto::S3 {
         /**
          * Part number
          */
-        int partNumber;
+        long partNumber{};
 
         /**
          * Content type
@@ -72,39 +73,87 @@ namespace AwsMock::Dto::S3 {
         std::string contentType;
 
         /**
-         * @brief Convert to a JSON string
-         *
-         * @return JSON string
-         */
-        [[nodiscard]] std::string ToJson() const;
-
-        /**
          * @brief Convert from XML representation
          *
          * @param xmlString XML string
          */
-        void FromXml(const std::string &xmlString);
+        void FromXml(const std::string &xmlString) {
+
+            try {
+
+                boost::property_tree::ptree pt;
+                read_xml(xmlString, pt);
+                checksumCrc32 = pt.get<std::string>("CompleteMultipartUpload.Part.ChecksumCRC32");
+                checksumCrc32c = pt.get<std::string>("CompleteMultipartUpload.Part.ChecksumCRC32C");
+                checksumSha1 = pt.get<std::string>("CompleteMultipartUpload.Part.ChecksumSHA1");
+                checksumSha256 = pt.get<std::string>("CompleteMultipartUpload.Part.ChecksumSHA256");
+                eTag = pt.get<std::string>("CompleteMultipartUpload.Part.ETag");
+                partNumber = pt.get<int>("CompleteMultipartUpload.Part.PartNumber");
+
+            } catch (std::exception &exc) {
+                log_error << exc.what();
+                throw Core::JsonException(exc.what());
+            }
+        }
 
         /**
-         * @brief Convert to a XMl string
+         * @brief Convert to an XML string
          *
          * @return XML string
          */
-        [[nodiscard]] std::string ToXml() const;
+        [[nodiscard]] std::string ToXml() const {
 
-        /**
-         * @brief Converts the DTO to a string representation.
-         *
-         * @return DTO as string
-         */
-        [[nodiscard]] std::string ToString() const;
+            try {
 
-        /**
-         * @brief Stream provider.
-         *
-         * @return output stream
-         */
-        friend std::ostream &operator<<(std::ostream &os, const CompleteMultipartUploadRequest &r);
+                boost::property_tree::ptree root;
+                root.add("CompleteMultipartUpload.Part.ETag", eTag);
+                root.add("CompleteMultipartUpload.Part.ChecksumCRC32", checksumCrc32);
+                root.add("CompleteMultipartUpload.Part.ChecksumCRC32C", checksumCrc32c);
+                root.add("CompleteMultipartUpload.Part.ChecksumSHA1", checksumSha1);
+                root.add("CompleteMultipartUpload.Part.ChecksumSHA256", checksumSha256);
+                root.add("CompleteMultipartUpload.Part.PartNumber", partNumber);
+                return Core::XmlUtils::ToXmlString(root);
+
+            } catch (std::exception &exc) {
+                log_error << exc.what();
+                throw Core::JsonException(exc.what());
+            }
+        }
+
+      private:
+
+        friend CompleteMultipartUploadRequest tag_invoke(boost::json::value_to_tag<CompleteMultipartUploadRequest>, boost::json::value const &v) {
+            CompleteMultipartUploadRequest r;
+            r.bucket = Core::Json::GetStringValue(v, "Bucket");
+            r.key = Core::Json::GetStringValue(v, "Key");
+            r.uploadId = Core::Json::GetStringValue(v, "UploadId");
+            r.checksumCrc32 = Core::Json::GetStringValue(v, "ChecksumCrc32");
+            r.checksumCrc32c = Core::Json::GetStringValue(v, "ChecksumCrc32c");
+            r.checksumSha1 = Core::Json::GetStringValue(v, "ChecksumSha1");
+            r.checksumSha256 = Core::Json::GetStringValue(v, "ChecksumSha256");
+            r.eTag = Core::Json::GetStringValue(v, "ETag");
+            r.partNumber = Core::Json::GetLongValue(v, "PartNunmber");
+            r.contentType = Core::Json::GetStringValue(v, "ContentType");
+            return r;
+        }
+
+        friend void tag_invoke(boost::json::value_from_tag, boost::json::value &jv, CompleteMultipartUploadRequest const &obj) {
+            jv = {
+                    {"Region", obj.region},
+                    {"User", obj.user},
+                    {"RequestId", obj.requestId},
+                    {"Bucket", obj.bucket},
+                    {"Key", obj.key},
+                    {"UploadId", obj.uploadId},
+                    {"ChecksumCrc32", obj.checksumCrc32},
+                    {"ChecksumCrc32c", obj.checksumCrc32c},
+                    {"ChecksumSha1", obj.checksumSha1},
+                    {"ChecksumSha256", obj.checksumSha256},
+                    {"ETag", obj.eTag},
+                    {"PartNumber", obj.partNumber},
+                    {"ContentType", obj.contentType},
+            };
+        }
     };
 
 }// namespace AwsMock::Dto::S3
