aaeccf16b72937dd2ff3f675313b19c644b53e61
jensv <jens.vogt@opitz-consulting.com>
2025-11-29 13:09:57 +0100

    fix: add installer packages

--- src/core/include/awsmock/core/Version.h
+++ src/core/include/awsmock/core/Version.h
@@ -1,11 +1,6 @@
-//~/work/awsmock/.git/hooks/
-// Created by vogje01 on 16/06/2023.
-//
+#pragma once
 
-#ifndef AWSMOCK_CORE_VERSION_H_IN_H
-#define AWSMOCK_CORE_VERSION_H_IN_H
-
-#define PROJECT_NAME "awsmock"
-#define PROJECT_VERSION "1.7.0"
-
-#endif //AWSMOCK_CORE_VERSION_H_IN_H
+#define APP_VERSION "1.7.0"
+#define APP_VERSION_MAJOR 1
+#define APP_VERSION_MINOR 7
+#define APP_VERSION_PATCH 0
