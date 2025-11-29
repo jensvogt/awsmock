f805de9e217406ed388093bbff71abb0da108d6a
Jens Vogt <jens.vogt@opitz-consulting.com>
2025-11-29 16:39:37 +0100

    feat(1234-new-ui): use the new UIDs

--- src/core/include/awsmock/core/Version.h
+++ src/core/include/awsmock/core/Version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define APP_NAME "awsmock"
-#define APP_VERSION "1.8.7"
+#define APP_VERSION "1.8.10"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 8
-#define APP_VERSION_PATCH 7
+#define APP_VERSION_PATCH 10
