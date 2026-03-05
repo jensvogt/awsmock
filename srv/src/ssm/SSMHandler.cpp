6ed5dc73e9c9abab9d4571a4ba46ec56fd95f6e4
Jens Vogt <jens.vogt@opitz-consulting.com>
2026-03-05 12:39:51 +0100

    fix: secrets manager tags

--- srv/src/ssm/SSMHandler.cpp
+++ srv/src/ssm/SSMHandler.cpp
@@ -37,7 +37,7 @@ namespace AwsMock::Service {
 
                     Dto::SSM::GetParameterCounterRequest ssmRequest = Dto::SSM::GetParameterCounterRequest::FromJson(clientCommand);
                     Dto::SSM::GetParameterCounterResponse ssmResponse = _ssmService.GetParameterCounter(ssmRequest);
-                    log_info << "Parameter found, name: " << ssmRequest.name << ", body: " << ssmResponse.ToJson();
+                    log_info << "Parameter found, name: " << ssmRequest.name;
                     return SendResponse(request, http::status::ok, ssmResponse.ToJson());
                 }
 
