beedb62d75cbc8a93dc0b0be15a502ca52f89f75
Jens Vogt <jens.vogt@opitz-consulting.com>
2026-05-26 21:48:04 +0200

    fix: add tests to github action

--- core/include/awsmock/core/Version.h
+++ core/include/awsmock/core/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.18.42"
+#define APP_VERSION "1.18.43"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 18
-#define APP_VERSION_PATCH 42
+#define APP_VERSION_PATCH 43
