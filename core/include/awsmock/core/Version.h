73fcbd90e2cae1da7a4623f207725f4ee3ec7387
Jens Vogt <jens.vogt@opitz-consulting.com>
2026-07-12 13:10:16 +0200

    fix: add logging to api gateway service, fix lambda invocation.

--- core/include/awsmock/core/Version.h
+++ core/include/awsmock/core/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.18.86"
+#define APP_VERSION "1.18.89"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 18
-#define APP_VERSION_PATCH 86
+#define APP_VERSION_PATCH 89
