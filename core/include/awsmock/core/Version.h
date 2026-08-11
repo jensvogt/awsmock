655df29b7b6a043ffb849617f87b4eda9f1e3015
jensv <jens.vogt@opitz-consulting.com>
2026-08-11 15:16:44 +0200

    fix: add controller file option

--- core/include/awsmock/core/Version.h
+++ core/include/awsmock/core/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION "1.18.105"
+#define APP_VERSION "1.18.109"
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 18
-#define APP_VERSION_PATCH 105
+#define APP_VERSION_PATCH 109
